32cd7b05adffb162de8b2b36083292a2c02c541e
FemBot 9000 <paige.el.olsen@gmail.com>
2021-11-19 12:16:28 -0800

    Finished rough draft of controllers

--- controllers/api/campaignController.js
+++ controllers/api/campaignController.js
@@ -4,27 +4,89 @@ const { Blog, User, Comment} = require('../../models');
 // The `http://localhost:3000/api/campaign` endpoint
 
 //CREATE CAMPAIGN
-
-
-
+router.post('/', async (req, res) => {
+    try {
+      const campaignData = await Campaign.create({
+        name: req.body.name,
+        gm_id: req.session.user.id,
+        user_id: [],  //Also not sure about this one
+        description: req.body.description,
+        picture: req.body.picture
+      })
+      res.status(200).json(campaignData)
+    } catch(err) {
+        console.log(err);
+        res.status(400).json({ message: "an error occured", err: err });
+      };
+  });
 
 //READ CAMPAIGN
+router.get('/', async (req, res) => {
+    try {
+      const campaignData = await Character.findAll({
+        include: [User, Character],
+      });
+      res.status(200).json(campaignData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
-
-
-
+  router.get('/:id', async (req, res) => {
+    try {
+      const campaignData = await Character.findByPk(req.params.id, {
+        include: [User, Character],
+      });
+  
+      if (!campaignData) {
+        res.status(404).json({ message: 'No Character found with that id!' });
+        return;
+      }
+  
+      res.status(200).json(campaignData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 //UPDATE CAMPAIGN
-
-
-
+router.put('/:id', async (req, res) => {
+    try {
+      const campaignData = await Character.update(req.body, {
+        where: {
+          id: req.params.id,
+        },
+      });
+      if (!campaignData[0]) {
+        res.status(404).json({ message: 'No Character with this id!' });
+        return;
+      }
+      res.status(200).json(campaignData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 
 //DELETE CAMPAIGN
-
-
-
-
+router.delete('/:id', async (req, res) => {
+    try {
+      const campaignData = await Character.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user.id
+        },
+      });
+      if (!campaignData) {
+        res.status(404).json({ message: 'No Character with this id!' });
+        return;
+      }
+      res.status(200).json(campaignData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
 
 
 module.exports = router;
