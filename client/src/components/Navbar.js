91d08378cfbfa8d8b28601d6d838e16ea47cfcdf
CarsdanDvorachek <cdvchek@gmail.com>
2021-11-19 14:49:29 -0500

    cleaned up code

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,24 +3,24 @@ function Navbar({ handlePageChange }) {
     return (
     <div className="container">
         <nav className="border row p-3 mb-5 mt-4 d-flex align-items-center" id="navbar">
-            <h1 className="col-8 text-start ps-4"
+            <h1 className="col-4 text-start ps-4"
                 onClick={()=> handlePageChange('home')}>
                 Table Top
             </h1>
-            <a className="col-1 text-center" id="profile-link"
-                onClick={()=> handlePageChange('profile')}>
-                Profile
-            </a>
-            <a className="col-1 text-center" id="community-link"
-                onClick={()=> handlePageChange('community')}>
-                Community
-            </a>
-            <a className="col-1 text-center" id="about-link"
-                onClick={()=> handlePageChange('about')}>
-                About
-            </a>
-            <div className="col-1 text-center">
-                <button className="btn"
+            <div className="col-8 text-end">
+                <button className="btn col-2 text-center" id="profile-link"
+                    onClick={()=> handlePageChange('profile')}>
+                    Profile
+                </button>
+                <button className="btn col-2 text-center" id="community-link"
+                    onClick={()=> handlePageChange('community')}>
+                    Community
+                </button>
+                <button className="btn col-2 text-center" id="about-link"
+                    onClick={()=> handlePageChange('about')}>
+                    About
+                </button>
+                <button className="btn col-2 text-center"
                     onClick={()=> handlePageChange('signup')}>
                     Signup
                 </button>
