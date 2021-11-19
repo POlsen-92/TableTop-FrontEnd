0097b6afb61ab4c543592e45258e9d1e9950e5c1
FemBot 9000 <paige.el.olsen@gmail.com>
2021-11-19 11:56:17 -0800

    adding controllers

--- controllers/api/characterController.js
+++ controllers/api/characterController.js
@@ -1,11 +1,21 @@
 const router = require('express').Router();
-const { Blog, User, Comment} = require('../../models');
+const { Character, User, Campaign} = require('../../models');
 
 // The `http://localhost:3000/api/character` endpoint
 
 //CREATE CHARACTER
-
-
+router.post('/', async (req, res) => {
+    try {
+      const characterData = await Character.create({
+        name: req.body.name,
+        user_id: req.session.user.id
+      })
+      res.status(200).json(blogData)
+    } catch(err) {
+        console.log(err);
+        res.status(400).json({ message: "an error occured", err: err });
+      };
+  });
 
 
 
