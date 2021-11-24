f7a00eeccf54de307b57f3154479e9faa6b49f8c
CarsdanDvorachek <cdvchek@gmail.com>
2021-11-24 18:08:19 -0500

    campaign navigation works

--- src/components/Pages/Campaign.js
+++ src/components/Pages/Campaign.js
@@ -1,18 +1,31 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom"
+import API from "../../utils/API";
 function Campaign(props) {
-    console.log(props)
+    const { id } = useParams();
     const gmID = 4;
     const userID = 3;
 
+    const [campaignName,setCampaignName] = useState('');
+    const [campaignDesc,setCampaignDesc] = useState('');
+
+    useEffect(() =>{
+        API.findCampaign(id,props.token).then((res)=>{
+            console.log(res);
+            setCampaignName(res.data.name);
+            setCampaignDesc(res.data.description)
+        })
+    },[])
     return (
     <div className="container">
-        <h1 className="row">Campaign Title</h1>
+        <h1 className="row">{campaignName}</h1>
         <div className="row">
             <button className="col-2 btn my-1 me-1">Launch Campaign</button>
             {(gmID === userID) ? (<button className="col-2 btn m-1">Edit Campaign</button>) : ""}
         </div>
         <div className="row">
-            <p className="border col-4 m-1">Campaign Description: This is the coolest campaign evaaaaar!!!!!</p>
+            <p className="border col-4 m-1">{campaignDesc}</p>
             <div className="border col-4 m-1 text-center">
                 <h2>GM</h2>
                 <h4>gm_username</h4>
