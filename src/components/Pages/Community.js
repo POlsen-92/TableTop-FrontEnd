778e79d72dc1893147f9219986c2e8e541a83d39
Marco <marco.shifflette@gmail.com>
2021-11-29 16:31:57 -0800

    adding conditional rendering to the blog page

--- src/components/Pages/Community.js
+++ src/components/Pages/Community.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios'
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import API from "../../utils/API"
 
 function Community({token,userState}) {
     const [posts, setPosts] = useState([])
-    
+
+    const navigate=useNavigate();
+
     console.log(posts);
     useEffect(()=>{
         axios.get('http://localhost:3001/api/blog/')
@@ -24,6 +26,7 @@ function Community({token,userState}) {
         API.deleteBlogPost(deletedPost,token).then((res) => {
             console.log(res);
             console.log("I deleted a Post!");
+            window.location.reload(false);
         })
     }
     // console.log(posts[0].User.username)
@@ -34,15 +37,22 @@ function Community({token,userState}) {
             {
                 posts.map((post) => {
                      return ( 
+                         <div>
                         <Link
                         to={{ pathname: `/BlogPost/${post.id}` }}
                         className="d-inline"
                       >
                          <div>
                      <li key={post.id}>{post.title}-{post.description}--{post.User.username}</li> 
-                     <button onClick = {(e) =>{deleteBlogPost(e.target.getAttribute("data-id"))}} data-id={post.id} >yrrrr</button>
                         </div> 
-                        </Link>                     
+                        </Link>
+                        {userState.username === post.User.username ? (
+                            <button onClick = {(e) =>{deleteBlogPost(e.target.getAttribute("data-id"))}} data-id={post.id} >yrrrr</button>
+                                
+                                ) : (
+                                    ''
+                                )}    
+                        </div>                 
                     )
             })}
         </ul>
