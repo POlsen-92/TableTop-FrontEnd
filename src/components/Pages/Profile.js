caeebe2bef3098621833791361c302ea72b9c8df
mark.lohsemiranda <mark.lohsemiranda@gmail.com>
2021-12-03 12:55:18 -0800

    Update Profile.js

--- src/components/Pages/Profile.js
+++ src/components/Pages/Profile.js
@@ -149,13 +149,13 @@ function Profile(props) {
   return (
     <div className="container">
       {/* USER USERNAME AND EMAIL */}
-      <div className="col-12" >
+      <div className="row" >
         <h1 className="text-center m-2">{username ? username : props.userState.username}</h1>
         <h2 className="text-center m-2">{email ? email : props.userState.email}</h2>
       </div>
       {/* CAMPAIGN INFORMATION FOR USER */}
       <div className="row text-center">
-        <section className="col-4 border" id="campaigns">
+        <section className="col-sm-12 col-md-4 border" id="campaigns">
           <h3>Your Campaigns</h3>
           <CampaignFilters
             handleCampaignFilterChange={handleCampaignFilterChange}
@@ -186,7 +186,7 @@ function Profile(props) {
           </button>
         </section>
         {/* USER INFORMATION - EDIT IMAGE, USERNAME, PASSWORD, EMAIL  */}
-        <section className="col-4" id="profile-info">
+        <section className="col-sm-12 col-md-4" id="profile-info">
           <img
             src={imageURL ? imageURL : props.userState.image_content}
             width="200"
@@ -245,7 +245,7 @@ function Profile(props) {
           <button className="btn m-1">Notifications</button>
         </section>
         {/* ALL CHARACTERS FOR USER */}
-        <section className="col-4 border" id="all-characters-list">
+        <section className="col-sm-12 col-md-4 border" id="all-characters-list">
           <h3>Your Characters</h3>
           <ul>
             {allMyCharacters.map((character)=>{
